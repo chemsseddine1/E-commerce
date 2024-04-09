2852d9dd97656d5c1fd1b1f521ac9f2128cb79fd
chemsseddine1 <abdouschemsseddine@gmail.com>
2024-04-09 12:55:33 +0100

    first commit

---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CarItems from "../Components/CartItems/CartItems";
+
+
+const Cart =() => {
+    return (
+        <div>
+          <CarItems/>
+        </div>
+    )
+}
+
+export default Cart
---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+import Offers from "../Components/Offers/Offers";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+
+const Shop =() => {
+    return (
+        <div>
+           <Hero/>
+           <Popular/>
+           <Offers/>
+           <NewCollections/>
+           <NewsLetter/>
+        </div>
+    )
+}
+
+export default Shop
